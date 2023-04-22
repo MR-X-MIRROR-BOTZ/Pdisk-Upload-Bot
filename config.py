e708714b6ba95800f9a2a03dd4a1a67c5705fde9
AshokChakravarthy666 <106798009+AshokChakravarthy666@users.noreply.github.com>
2023-04-22 22:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     API_HASH = os.environ.get("API_HASH", "7cce51d4664d010b90ad690e0d5121ad")
     
-    API_KEY = os.environ.get("API_KEY", "https://pdisk.pro/api/account/info?key=6994vx27wa0sin4z1di")
+    API_KEY = os.environ.get("API_KEY", "6994vx27wa0sin4z1di")
 
     # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
 
