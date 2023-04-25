43a145013471c0d9bdcd526ef5eec2f962251db5
MR X MIRROR <127676677+MR-X-MIRROR-BOTZ@users.noreply.github.com>
2023-04-25 19:06:09 +0530

    Update pdisk.py

--- plugins/pdisk.py
+++ plugins/pdisk.py
@@ -42,7 +42,7 @@ async def pdisk(bot, message):
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
             r = requests.get(auth,headers)
             res = r.json()
-            #print(res)
+            print(res)
             id = res["data"]["item_id"]
             await message.reply_chat_action("typing")
             pdisk = "https://pdisk.pro/share-video?videoid="+id      
