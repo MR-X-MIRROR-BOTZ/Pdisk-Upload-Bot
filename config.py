c9a13883028521bebcfef9b0a5d264acfb629dc5
MR X MIRROR <127676677+MR-X-MIRROR-BOTZ@users.noreply.github.com>
2023-04-25 17:35:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6073397510:AAHa8bAlNwuNrPVRUMBwjKlw-3_eh5sfdus")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6175306650:AAEdiR3_bJkSKVv0ZqKmxnBgNu-NmYyiXUU")
 
-    API_ID = int(os.environ.get("API_ID", "15657755"))
+    API_ID = int(os.environ.get("API_ID", "5397731"))
 
-    API_HASH = os.environ.get("API_HASH", "7cce51d4664d010b90ad690e0d5121ad")
+    API_HASH = os.environ.get("API_HASH", "051ebba43e161aa6f6456af524bad699")
     
-    API_KEY = os.environ.get("API_KEY", "6994vx27wa0sin4z1di")
+    API_KEY = os.environ.get("API_KEY", "4644exhpide5yevfy4n8")
 
     # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
 
